06a12a266cdd43e00a0e6884fc36825f233d7ba2
Nguyen Quoc Dat <datsquall@gmail.com>
2016-11-20 02:46:35 +0200

    Frontend invitation works

--- TurnUp/TurnUp/urls.py
+++ TurnUp/TurnUp/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from business.views import rsvp
 
 urlpatterns = [
     url(r'', include('business.urls')),
+    url(r'^rsvp', rsvp),
     url(r'^business/', include('business.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- TurnUp/business/views.py
+++ TurnUp/business/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 import json
-from django.http import HttpResponseRedirect, HttpResponse
+from firebase import firebase
+import os
+from TurnUp.settings import BASE_DIR
 
+with open(os.path.join(BASE_DIR, 'TurnUp/auth.json')) as auth_file:
+    auth_info = json.loads(auth_file.read())
+    firebase_secret = auth_info.get('firebase_secret')
+    firebase_user= auth_info.get('firebase_user')
+    firebase_url= auth_info.get('firebase_url')
+    admin_uid = auth_info.get('admin_uid')
 
 # Business index
 def index(request):
@@ -11,3 +19,23 @@ def index(request):
 def food(request):
     context = {}
     return render(request, 'business/food.html', context)
+
+def rsvp(request):
+    context = {}
+    rsvp_data = json.loads(request.body)
+    u = rsvp_data.get('u')
+    e = rsvp_data.get('e')
+    i = rsvp_data.get('i')
+    if not (u and e and i):
+        return render(request, 'business/index.html', context)
+
+    attending = rsvp_data.get('attending')
+    common_availability = rsvp_data.get('commonAvailability')
+
+    auth = firebase.FirebaseAuthentication(firebase_secret,
+                                           firebase_user, extra={'uid': admin_uid})
+    fb = firebase.FirebaseApplication(firebase_url, authentication=auth)
+    invitee = fb.get('/events/%s/%s/invitees' % (u, e), i)
+    if invitee:
+        fb.patch('/events/%s/%s/invitees/%s' % (u, e, i), {'attending': attending, 'commonAvailability': common_availability})
+    return render(request, 'business/index.html', context)
